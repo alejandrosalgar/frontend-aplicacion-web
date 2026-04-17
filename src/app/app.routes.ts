1b5ff50df1f068d19b8bc0a5ab3c43214087aa75
dayanapiedrahita <dayanapiedrahita98@gmail.com>
2026-04-16 19:15:24 -0500

    feat: Estrctura inicial del front

---
+++ src/app/app.routes.ts
@@ -0,0 +1,52 @@
+import { Routes } from '@angular/router';
+
+import { auditUserGuard } from '../../../../Proyecto Banco/frontend-aplicacion-web/src/app/core/audit-user.guard';
+
+//COMPLETAR SEGUN ENTIDADES 
+export const routes: Routes = [
+//   { path: '', redirectTo: 'login', pathMatch: 'full' },
+//   {
+//     path: 'login',
+//     loadComponent: () => import('../../../../Proyecto Banco/frontend-aplicacion-web/src/app/features/login/login').then((m) => m.LoginComponent),
+//   },
+//   {
+//     path: 'app',
+//     canActivate: [auditUserGuard],
+//     loadComponent: () => import('../../../../Base front/frontend-programacion-de-software/web/src/app/features/shell/main-layout').then((m) => m.MainLayoutComponent),
+//     children: [
+//       { path: '', redirectTo: 'usuarios', pathMatch: 'full' },
+//       {
+//         path: 'usuarios',
+//         loadComponent: () =>
+//           import('../../../../Base front/frontend-programacion-de-software/web/src/app/features/usuarios/usuario-list').then((m) => m.UsuarioListComponent),
+//       },
+//       {
+//         path: 'categorias',
+//         loadComponent: () =>
+//           import('../../../../Base front/frontend-programacion-de-software/web/src/app/features/categorias/categoria-list').then((m) => m.CategoriaListComponent),
+//       },
+//       {
+//         path: 'productos',
+//         loadComponent: () =>
+//           import('../../../../Base front/frontend-programacion-de-software/web/src/app/features/productos/producto-list').then((m) => m.ProductoListComponent),
+//       },
+//       {
+//         path: 'pedidos',
+//         loadComponent: () =>
+//           import('../../../../Base front/frontend-programacion-de-software/web/src/app/features/pedidos/pedido-list').then((m) => m.PedidoListComponent),
+//       },
+//       {
+//         path: 'detalles-pedido',
+//         loadComponent: () =>
+//           import('../../../../Base front/frontend-programacion-de-software/web/src/app/features/detalles-pedido/detalle-pedido-list').then(
+//             (m) => m.DetallePedidoListComponent,
+//           ),
+//       },
+//       {
+//         path: 'pagos',
+//         loadComponent: () => import('../../../../Base front/frontend-programacion-de-software/web/src/app/features/pagos/pago-list').then((m) => m.PagoListComponent),
+//       },
+//     ],
+//   },
+//   { path: '**', redirectTo: 'login' },
+];
